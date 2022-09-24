32cdc3d75549b48af284a2361ee869a5bf74d8ab
Pavel365 <pavelzhilkov@abv.bg>
2022-09-25 01:58:30 +0300

    Add file

---
+++ GuessANumber/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace GuessANumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomNumber = new Random();
+            int computerNumber = randomNumber.Next(1,101);
+
+            while (true)
+            {
+                Console.Write("Guess a number (1-100): ");
+                string playerInput = Console.ReadLine();
+                bool isValid = int.TryParse(playerInput, out int playerNumber);
+
+                if (isValid)
+                {
+                    if (playerNumber == computerNumber)
+                    {
+                        Console.WriteLine("You guessed it!");
+                        break;
+                    }
+                    else if (playerNumber > computerNumber)
+                    {
+                        Console.WriteLine("Too High");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Too Low");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input.");
+                }
+            }
+        }
+    }
+}
